8744b3df2c99080090ca2237d063069b7712e05e
Mihaela Budeanu <mihaela.budeanu@endava.com>
2024-12-21 21:11:32 +0200

    - added a new class and converted the temperature, from Fahnrenheit to Celsius

---
+++ src/test/java/Homework/ConvertFahnrenheitToCelsius.java
@@ -0,0 +1,19 @@
+package Homework;
+
+import org.testng.annotations.Test;
+
+public class ConvertFahnrenheitToCelsius {
+
+    public double fahrenheit;
+    public double celsius;
+
+    @Test
+    public void convertFahrenheitToCelsius(){
+        fahrenheit = -92;
+        celsius = (fahrenheit - 32) * 5/9;
+        System.out.println("The temperature in Fahrenheit is: " + fahrenheit);
+        System.out.println("The temperature in Celsius is: " + celsius);
+
+    }
+
+}
