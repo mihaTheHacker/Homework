aef158278379a7a4bcd6f7b5b01c5b6d14fa24f7
Mihaela Budeanu <mihaela.budeanu@endava.com>
2024-12-22 21:31:25 +0200

    - created a new branch - added and printed a new class with a boolean value

---
+++ src/test/java/Homework/BooleanValueAssignmentAndPrinting.java
@@ -0,0 +1,15 @@
+package Homework;
+
+import org.testng.annotations.Test;
+
+public class BooleanValueAssignmentAndPrinting {
+    //Create a program that declares a boolean variable, assigns a value to it, and then prints it.
+
+    boolean myBoolean = true;
+
+    @Test
+    public void booleanValueAssignmentAndPrinting() {
+        System.out.println("The boolean value is: " + myBoolean);
+    }
+
+}
