194aa5a6d13f4a35efbc83d05e8de2cc0c08d5fe
Mihaela Budeanu <mihaela.budeanu@endava.com>
2025-01-14 20:34:59 +0200

    - added a new class where I compared 2 integers

---
+++ src/test/java/Homework/BooleanComparison.java
@@ -0,0 +1,23 @@
+package Homework;
+
+import org.testng.annotations.Test;
+
+public class BooleanComparison {
+
+    //Create a program that compares two integers using relational operations and stores the result in a boolean variable.
+    //Then print the boolean variable.
+    @Test
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        boolean result1 = a > b;
+        boolean result2 = a < b;
+        boolean result3 = a == b;
+        boolean result4 = a != b;
+        System.out.println("The result of the comparison is: " + result1);
+        System.out.println("The result of the comparison is: " + result2);
+        System.out.println("The result of the comparison is: " + result3);
+        System.out.println("The result of the comparison is: " + result4);
+    }
+
+}
