b14fa30e93fb899322da56faa5bf9c937688575b
Mihaela Budeanu <mihaela.budeanu@endava.com>
2025-01-17 21:30:57 +0200

    - I created a new package "OperatorsIfBlocksAndSwitchBreakAndContinueExercises" - I created a new branch "Operators" - I created a new class SumOfTwoIntegers.java - solved the first exercise

---
+++ src/test/java/OperatorsIfBlocksAndSwitchBreakAndContinueExercises/SumOfTwoIntegers.java
@@ -0,0 +1,16 @@
+package OperatorsIfBlocksAndSwitchBreakAndContinueExercises;
+
+import org.testng.annotations.Test;
+
+public class SumOfTwoIntegers {
+
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+
+    @Test
+    public void sumOfTwoIntegers() {
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
+
