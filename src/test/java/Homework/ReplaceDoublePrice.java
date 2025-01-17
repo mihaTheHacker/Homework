3c0efc93ed7e181fa7e272f26f2febaeed4b69d1
Mihaela Budeanu <mihaela.budeanu@endava.com>
2025-01-17 14:53:47 +0200

    - I created a new class ReplaceDoublePrice.java - I created a new exercise in which I replaced the double value 12.99 (as a String) with a new value 7.99

---
+++ src/test/java/Homework/ReplaceDoublePrice.java
@@ -0,0 +1,21 @@
+package Homework;
+
+import org.openqa.selenium.json.JsonOutput;
+import org.testng.annotations.Test;
+
+public class ReplaceDoublePrice {
+
+    public String originalOffer = "12.99 euros";
+    public String oldPrice = "12.99";
+    public String newPrice = "7.99";
+
+    public String updatedOffer = originalOffer.replace(oldPrice, newPrice);
+
+    @Test
+    public void replaceDoublePrice() {
+        System.out.println("It was: " + originalOffer);
+        System.out.println("Discounted offer is: " + updatedOffer);
+        System.out.println(originalOffer.replace(oldPrice, newPrice));
+    }
+
+}
