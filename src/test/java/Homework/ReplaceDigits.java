ba424940a4f6cda292b7a7b221bf1f79bb6c078e
Mihaela Budeanu <mihaela.budeanu@endava.com>
2025-01-17 14:23:29 +0200

    - I declared a new variable newDigitsString - I added 2 extra commands to print both the original and the modified information to the console

--- src/test/java/Homework/ReplaceDigits.java
+++ src/test/java/Homework/ReplaceDigits.java
@@ -7,9 +7,12 @@ public class ReplaceDigits {
     String digitString = "123456789";
     char oldChar = '1';
     char newChar = '0';
+    String newDigitString = digitString.replace(oldChar, newChar);
 
     @Test
     public void replaceDigits() {
+        System.out.println("Original digit string is: " + digitString);
+        System.out.println("New digit string is: " + newDigitString);
         System.out.println(digitString.replace(oldChar, newChar));
     }
 }
