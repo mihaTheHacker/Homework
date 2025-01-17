65edce73b6a43388f2377546054d8e239eb25888
Mihaela Budeanu <mihaela.budeanu@endava.com>
2025-01-17 21:40:26 +0200

    - I created a new class DifferenceOfTwoIntegers.java - I solved the exercise

---
+++ src/test/java/OperatorsIfBlocksAndSwitchBreakAndContinueExercises/DifferenceOfTwoIntegers.java
@@ -0,0 +1,16 @@
+package OperatorsIfBlocksAndSwitchBreakAndContinueExercises;
+
+import org.testng.annotations.Test;
+
+public class DifferenceOfTwoIntegers {
+    int a = 879;
+    int b = 234;
+    int difference = a - b;
+
+    @Test
+
+    public void differenceOfTwoIntegers() {
+        System.out.println("The difference of " + a + " and " + b + " is: " + difference);
+    }
+
+}
